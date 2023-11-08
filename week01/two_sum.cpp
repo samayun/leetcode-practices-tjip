9c48166791ff98b5558991aef477bb5c3ab9027a
Samayun Chowdhury <samayun.m.chowdhury@gmail.com>
2023-11-09 00:51:53 +0600

    feat(twoSum): solve it

---
+++ week01/two_sum.cpp
@@ -0,0 +1,16 @@
+// TC: O(N)
+// MC: O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashMap;
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (hashMap.find(diff)!= hashMap.end()) {
+                return { hashMap[diff], i };
+            }
+            hashMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
